33e74f2df8ec1170f9aba239d95d3399ffdf9ae1
Harry1212 <118365419+Harry1212159@users.noreply.github.com>
2023-11-05 22:20:21 +0700

    update new code

--- 201.cpp
+++ 201.cpp
@@ -1,17 +1,19 @@
 #include <string>
 #include <iostream>
-#include <stdlib.h>
-#include <algorithm>
+
 using namespace std;
 
-int charToInt(char c){
+int charToInt(char c, int B){
+    int res = -1;
     if(c >= '0' && c <= '9')
         return c - '0';
     else if(c >= 'A' && c <= 'Z')
         return c - 'A' + 10;
-    else    
-        return -1;
+    if (res >= 0 && res < B) return res;
+    throw::invalid_argument("Not an int in base B");
+    return -1;
 }
+
 string addToBaseB(string s1, string s2, int B){
     string result = "";
     int carry = 0;
@@ -19,8 +21,8 @@ string addToBaseB(string s1, string s2, int B){
     int len2 = s2.length();
     int maxLen = max(len1, len2);
     for(int i = 0; i < maxLen; i++){
-        int val1 = (i < len1) ? charToInt(s1[len1 - i - 1]) : 0;
-        int val2 = (i < len2) ? charToInt(s2[len2 - i - 1]) : 0;
+        int val1 = (i < len1) ? charToInt(s1[len1 - i - 1], B) : 0;
+        int val2 = (i < len2) ? charToInt(s2[len2 - i - 1], B) : 0;
         int sum = val1 + val2 + carry;
         carry = sum / B;
         sum = sum % B;
@@ -33,28 +35,37 @@ string addToBaseB(string s1, string s2, int B){
     return result;
 }
 
-int isGreater(string s1, string s2){
-    int min = ((s1.size() < s2.size()) ? s1.size() : s2.size());
-    for(int i = 0; i < min; i++){
-        if(s1[i] > s2[i]) return 1;
-        else if(s1[i] < s2[i])  return -1;
+bool isGreater(const std::string& s1, const std::string& s2) {
+    int len1 = s1.length();
+    int len2 = s2.length();
+
+    if (len1 < len2) {
+        return false;
+    } else if (len1 > len2) {
+        return true;
     }
-    if(s1.size() > s2.size()) return 1;
-    else if(s1.size() < s2.size()) return -1;
-    else return 0;
+
+    for (int i = 0; i < len1; ++i) {
+        if (s1[i] < s2[i]) {
+            return false;
+        } else if (s1[i] > s2[i]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 string subtractToBaseB(string s1, string s2, int B){
     string result = "";
-    if(isGreater(s1, s2) == -1) return "-" + subtractToBaseB(s2, s1, B);
+    if(!isGreater(s1, s2)) return "-" + subtractToBaseB(s2, s1, B);
     int carry = 0;
     int len1 = s1.length();
     int len2 = s2.length();
     int maxLen = max(len1, len2);
     int borrow = 0;
     for (int i = 0; i < maxLen; i++) {
-        int value1 = (i < len1) ? charToInt(s1[len1 - i - 1]) : 0;
-        int value2 = (i < len2) ? charToInt(s2[len2 - i - 1]) : 0;
+        int value1 = (i < len1) ? charToInt(s1[len1 - i - 1], B) : 0;
+        int value2 = (i < len2) ? charToInt(s2[len2 - i - 1], B) : 0;
         value1 -= borrow;
 
         if (value1 < value2) {
@@ -80,17 +91,19 @@ int main(){
 
     cout << "Enter base (2-36): ";
     cin >> B;
+    cin.ignore();
 
     if (B < 2 || B > 36) {
         cout << "Invalid base. Please enter a valid base between 2 and 36." << endl;
         return 1;
     }
 
-    cout << "Enter first number X in base " << B << ": ";
+    cout << "Enter first number X in base " << B << " (X >= 0): ";
     cin >> X;
-    cout << "Enter second number Y in base " << B << ": ";
+    
+    cout << "Enter second number Y in base " << B << " (Y >= 0): ";
     cin >> Y;
-
+    
     string sum = addToBaseB(X, Y, B);
     string difference = subtractToBaseB(X, Y, B);
     cout << "Sum: " << sum << endl;
